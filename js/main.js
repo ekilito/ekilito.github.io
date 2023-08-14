84fd7e1d7212d072dcd2ddd4a35de22bb6d14fdd
ekilito <2856509748@qq.com>
2023-08-15 02:21:45 +0800

    Site updated: 2023-08-15 02:21:44

--- js/main.js
+++ js/main.js
@@ -599,6 +599,7 @@ document.addEventListener("DOMContentLoaded", function () {
     let initTop = 0;
     let isChatShow = true;
     const $header = document.getElementById("page-header");
+    const $popupWindow = document.getElementById('popup-window')
     const isChatBtnHide = typeof chatBtnHide === "function";
     const isChatBtnShow = typeof chatBtnShow === "function";
 
@@ -672,7 +673,18 @@ document.addEventListener("DOMContentLoaded", function () {
         // ignore small scrolls
         return;
       }
+      if ($popupWindow && $popupWindow.classList.contains('show-popup-window') && currentTop > 60 && delta > 20 && lastScrollTop != 0) {
+        // 滚动后延迟1s关闭弹窗
+        setTimeout(() => {
+          $popupWindow.classList.add('popup-hide');
+          setTimeout(() => {
+            $popupWindow.classList.remove('popup-hide');
+            $popupWindow.classList.remove('show-popup-window');
+          }, 1000)
+        }, 1000);
+      }
       lastScrollTop = currentTop;
+
       if (currentTop > 26) {
         if (isDown) {
           if ($header.classList.contains("nav-visible")) $header.classList.remove("nav-visible");
